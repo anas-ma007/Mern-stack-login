051947b98d7d9f37fe0df19e54060d999213e859
anas-ma007 <anaschelakkara786@gmail.com>
2023-07-18 15:37:10 +0530

    Add Frontend UI

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { FaSignOutAlt } from "react-icons/fa";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignOutAlt /> Login
+        </h1>
+
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="Your Email"
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Enter Password"
+                onChange={onChange}
+              />
+            </div>{" "}
+            
+            <div className="from-group">
+              <button type="submit" className="btn btn-block">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+      </section>
+    </>
+  );
+}
+
+export default Register;
